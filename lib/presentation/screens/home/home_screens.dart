bbb74cc7ce2fc0339a3c7642cc5da1a95726246d
Juan Esteban <estebancode09@gmail.com>
2023-05-08 17:43:34 -0500

    Title: Update App, Description: Home Screen implemented

--- lib/presentation/screens/home/home_screens.dart
+++ lib/presentation/screens/home/home_screens.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
+
+ 
   const HomeScreen({super.key});
 
   @override
@@ -9,7 +12,41 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
          title: const Text('Flutter + Material 3'),
       ),
-      body: const Center(child: Text('Home Screen') ),
+      body: _HomeView()
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+   
+
+  @override
+  Widget build(BuildContext context) => ListView.builder(
+      
+      itemCount: appMenuItems.length, 
+      itemBuilder:( BuildContext context , int index) {
+        final menuItem = appMenuItems[index]; 
+       return _CustomListTile(menuItem: menuItem);
+      },
+  );
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+     title: Text(menuItem.title),
+     subtitle: Text(menuItem.subtitle),
+     leading:  Icon(menuItem.icon, color: colors.primary,),
+     trailing: Icon(Icons.keyboard_arrow_right,color: colors.primary ),
+     onTap: (){},
     );
   }
 }
