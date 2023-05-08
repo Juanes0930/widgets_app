1e5e6f3bedf28b91eb9de98308e4f4918912100a
Juan Esteban <estebancode09@gmail.com>
2023-05-08 15:55:11 -0500

    Title: Update App , Description: Created menu items

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MenuItem{
+
+final String title; 
+final String subtitle;
+final String link;
+final IconData icon;
+
+MenuItem({required this.title , required this.subtitle , required this.link , required this.icon});
+
+
+}
+
+ List<MenuItem> appMenuItems = <MenuItem> [
+  MenuItem(
+    title: 'Buttons', 
+    subtitle: 'buttons in flutter' , 
+    link: '/buttons', 
+    icon: Icons.smart_button_outlined),
+    
+  MenuItem(
+    title: 'Cards', 
+    subtitle: 'Container stylized', 
+    link: '/card', 
+    icon: Icons.credit_card)  
+];
